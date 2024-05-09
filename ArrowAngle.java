e8fe86e577a3bef3eb86e47d83b8e4b371c1c40b
Ethan Owen Taruc <ethan.taruc@obf.ateneo.edu>
2024-05-09 19:30:06 +0800

    AngleDirection Finished// To do PowerBar

--- ArrowAngle.java
+++ ArrowAngle.java
@@ -2,82 +2,98 @@ import javax.swing.ImageIcon;
 
 public class ArrowAngle extends GameObject {
     int angle;
-    boolean isMaxAngle;
-    boolean isMinAngle;
+    boolean willGoDown;
+    boolean willGoUp;
 
     public ArrowAngle(int x, int y, ImageIcon image) {
-        super(x, y, image);   
+        super(x, y, image);
     }
 
     public void generateAngle(Player player) {
         if (player.playerPosition()){
             angle = (int) (Math.random() * 180 + 90); //90-270
             setAngle(angle);
+            goUp();
         } else {
             angle = (int) (Math.random() * 180 - 90); //90 - -90
             setAngle(angle);
+            goUp();
         }
-        isMaxAngle(player);
+        isUpOrDown(player);
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+    
+    public void isUpOrDown(Player player) {
+        if (player.playerPosition()) {
+            if (angle == 270) {
+                goDown();
+            } else if (angle == 90) {
+                goUp();
+            }
+        } else {
+            if (angle == 90) {
+                goDown();
+            } else if (angle == -90) {
+                goUp();
+            }
+        }
+    }
+
+    public void rotateAngle(Player player) {
+        if (willGoUp) {
+            rotateAngleUp(player);
+        } else if (willGoDown) {
+            rotateAngleDown(player);
+        }
     }
 
     private void setAngle(int angle){
         this.angle = angle;
     }
 
-    public int getAngle() {
-        return angle;
+    private void goUp(){
+        willGoUp = true;
+        willGoDown = false;
     }
 
-    public void isMaxAngle(Player player) {
-        if (player.playerPosition()) {
-            if (angle == 270) {
-                isMaxAngle = true;
-            } else {
-                isMaxAngle = false;
-            }
-        } else {
-            if (angle == 90) {
-                isMaxAngle = true;
-            } else {
-                isMaxAngle = false;
-            }
-        }
+    private void goDown(){
+        willGoDown = true;
+        willGoUp = false;
     }
 
-    public void rotateAngleUp(Player player) {
+    private void rotateAngleUp(Player player) {
         if (player.playerPosition()){
-            if (270>angle && !isMaxAngle) {
+            if (270>angle && angle>90) {
                 angle++;
-                isMaxAngle(player);
             } else {
                 angle = 270;
             }
         } else {
-            if (90>angle && !isMaxAngle) {
+            if (90>angle && angle>-90) {
                 angle++;
-                isMaxAngle(player);
             } else {
                 angle = 90;
             }
         }
     }
 
-    public void isMinAngle(Player player) {
-        if (player.playerPosition()) {
-            if (angle == 90) {
-                isMinAngle = true;
+
+    private void rotateAngleDown(Player player) {
+        if (player.playerPosition()){
+            if (270>angle && angle>90) {
+                angle--;
             } else {
-                isMinAngle = false;
+                angle = 90;
             }
         } else {
-            if (angle == -90) {
-                isMinAngle = true;
+            if (90>angle && angle>-90) {
+                angle--;
             } else {
-                isMinAngle = false;
+                angle = -90;
             }
         }
-    }
-
-    public void rotateAngleDown() {
     }
 }
